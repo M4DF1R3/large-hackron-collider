79f5d3389bc1d95a562ff5adc123d7e3c3a5dd79
t-mannan <88060835+t-mannan@users.noreply.github.com>
2023-01-07 16:41:30 -0700

    Collision detection works, so does transparency

--- src/main.js
+++ src/main.js
@@ -1,22 +1,75 @@
+//set up scene
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
+//set up camera
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const cameraDist = 200;
+let theta = 0;
+camera.position.z = cameraDist;
+
+//set up light source
+var light = new THREE.AmbientLight(0xffffff);
+light.position.set(0, 1, 1).normalize();
+scene.add(light);
+
+//set up renderer
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-const cube = new THREE.Mesh(geometry, material);
+//add walls box
+const wallSize = 150;
+const wall_box = new THREE.BoxGeometry(wallSize, wallSize, wallSize);
+const wallMaterial = new THREE.MeshMatcapMaterial({ color: "#bbbbbb", transparent: true, opacity: 0.5 });
+const walls = new THREE.Mesh(wall_box, wallMaterial);
+scene.add(walls);
+
+//add 2 spheres
+const geometry = new THREE.SphereGeometry(10, 32, 16);
+const atomMaterial = new THREE.MeshMatcapMaterial({ color: "cyan" });
+const sphere1 = new THREE.Mesh(geometry, atomMaterial);
+const sphere2 = new THREE.Mesh(geometry, atomMaterial)
+let velocity = new THREE.Vector3(3, 4, 0);
+scene.add(sphere1);
+scene.add(sphere2);
+
+// create bounding boxes for the spheres
+const box1 = new THREE.Box3().setFromObject(sphere1);
+const box2 = new THREE.Box3().setFromObject(sphere2);
 
 
-scene.add(cube);
+// rotates the camera
+function moveCamera() {
+    theta += 0.002;
+    camera.position.x = cameraDist * Math.sin(theta);
+    camera.position.z = cameraDist * Math.cos(theta);
+    camera.lookAt(0, 0, 0);
+}
 
-camera.position.z = 5;
+
+let phase = 0
+// animation loop
 function animate() {
     requestAnimationFrame(animate);
     renderer.render(scene, camera);
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
+    moveCamera()
+    sphere1.position.x = -5 + 20 * Math.sin(phase);
+    sphere2.position.x = 5 - 20 * Math.sin(phase);
+
+    // update the bounding boxes
+    box1.setFromObject(sphere1);
+    box2.setFromObject(sphere2);
+
+    // check if the bounding boxes intersect
+    if (box1.intersectsBox(box2)) {
+        // perform a more precise check to see if the spheres are colliding
+        const distSquared = sphere1.position.distanceToSquared(sphere2.position);
+        const radiusSum = 20; // sum of the radii of the two spheres
+        if (distSquared < radiusSum * radiusSum) {
+            console.log('spheres are colliding');
+        }
+    }
+
+    phase+= 0.01
 }
 animate();
